d62bccd106ee523e642224af6a5a6d520f947658
sagor155670 <sagor@braincraftapps.com>
2023-10-22 11:45:04 +0600

    Initial Commit

---
+++ SwiftUiCodeOrganizer/SwiftUiCodeOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiCodeOrganizerApp.swift
+//  SwiftUiCodeOrganizer
+//
+//  Created by Saiful Islam Sagor on 22/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiCodeOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
